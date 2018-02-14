634b110b681172ee6617fd0b1753e1b793fd933a
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-13 20:27:36 -0700

    Add router for / to splash

---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Splash.css';
+
+class Splash extends Component {
+  render() {
+    return (
+      <div>
+        <p>quotes</p>
+      </div>
+    )
+  }
+}
+
+export default Splash;
