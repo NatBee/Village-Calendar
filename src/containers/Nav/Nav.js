0baa2db6b8e8870f641d32bb19a9e695b3181df5
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-18 09:02:42 -0700

    Add functionality to remove token after logout

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { logOutUser, removeAllEvents } from '../../actions/index';
+import { logOutUser, removeAllEvents, removeToken } from '../../actions/index';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import './Nav.css';
@@ -12,7 +12,8 @@ class Nav extends Component {
     firebase.auth().signOut();
     const user = this.props.user;
     this.props.logOutUser(user);
-    this.props.removeAllEvents(this.props.events)
+    this.props.removeAllEvents(this.props.events);
+    this.props.removeToken(this.props.token);
     this.props.history.push('/');
   }
 
@@ -35,12 +36,14 @@ class Nav extends Component {
 }
   export const mapStateToProps = (store) => ({
     user: store.user,
-    events: store.events
+    events: store.events,
+    token: store.token
   })
 
   export const mapDispatchToProps = (dispatch) => ({
     logOutUser: (user) => dispatch(logOutUser(user)),
-    removeAllEvents: (events) => dispatch(removeAllEvents(events))
+    removeAllEvents: (events) => dispatch(removeAllEvents(events)),
+    removeToken: (token) => dispatch(removeToken(token))
   })
 
 
