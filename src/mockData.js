5a01cc02a299c2ce6e51852f8b5fa13254e91431
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-21 13:43:11 -0700

    Add mock data file and complete calendarEventReducer test

---
+++ src/mockData.js
@@ -0,0 +1,33 @@
+export const mockData = {
+  user: {
+    user: 'Dk', 
+    credential: 'Xg', 
+    additionalUserInfo: 'Pg', 
+    operationType: "signIn"
+  },
+  events: [
+    {
+      start: "2018-02-09T09:00:00-07:00",
+      end: "2018-02-09T10:00:00-07:00",
+      title: "Super Fun Party",
+      description: "Fun with friends",
+      eventID: "7js6e24fo0kin4qcotoegu37hg",
+      location: 'Mi Casa'
+    }
+  ],
+  token: 'token_number',
+  calendarID: 'calendarID',
+  village: [{natalie: 'nataliesmontoya@gmail.com'}],
+  time: {
+    startTime: "2018-02-28T19:00:00.000Z", 
+    endTime: "2018-02-28T20:00:00.000Z"
+  },
+  event: {
+    start: "2018-02-09T09:00:00-07:00",
+    end: "2018-02-09T10:00:00-07:00",
+    title: "Super Fun Party",
+    description: "Fun with friends",
+    eventID: "7js6e24fo0kin4qcotoegu37hg",
+    location: 'Mi Casa'
+  }
+}
